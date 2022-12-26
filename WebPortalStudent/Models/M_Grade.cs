2d53ebe9c8a705dce3b33f494a447c20c8c02493
MinhThu2001 <120347883+MinhThu111@users.noreply.github.com>
2022-12-26 14:51:38 +0700

    done  grade

---
+++ WebPortalStudent/Models/M_Grade.cs
@@ -0,0 +1,15 @@
+﻿namespace WebPortalStudent.Models
+{
+    public class M_Grade:M_BaseModel.BaseCustom
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string gradeCode { get; set; }
+        /// <summary>
+        /// Id lấy từ person. Xác định GVCN
+        /// </summary>
+        public int? teacherId { get; set; }
+        public string remark { get; set; }
+        public M_Person teacherObj { get; set; }
+    }
+}
---
+++ WebPortalStudent/Services/S_Grade.cs
@@ -0,0 +1,24 @@
+﻿using WebPortalStudent.Lib;
+using WebPortalStudent.Models;
+using System;
+
+namespace WebPortalStudent.Services
+{
+    public interface IS_Subject
+    {
+        Task<ResponseData<List<M_Subject>>> getListSubject(string accessToken);
+    }
+    public class S_Subject : IS_Subject
+    {
+        private readonly ICallBaseApi _callApi;
+        public S_Subject(ICallBaseApi callApi)
+        {
+            _callApi = callApi;
+        }
+        public async Task<ResponseData<List<M_Subject>>> getListSubject(string accessToken)
+        {
+            return await _callApi.GetResponseDataAsync<List<M_Subject>>("/Subject/getListSubject", default(Dictionary<string, dynamic>), accessToken);
+        }
+     
+    }
+}
